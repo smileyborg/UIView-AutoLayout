b0656f8c9c9d547748b17098173d3fa2e111f542
Tyler Fox <tfox@smileyborg.com>
2013-08-20 22:02:11 -0700

    Add initial suite of unit tests to project

---
+++ Tests/AutoLayout/ALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALAppDelegate.h
+//  AutoLayout
+//
+//  Copyright (c) 2013 Tyler Fox
+//  https://github.com/smileyborg/UIView-AutoLayout
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
